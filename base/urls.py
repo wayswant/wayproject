8843c111f72ec75bd4560fc1f56e0ce2f829e941
wahyukuswant <97098665+wahyukuswant@users.noreply.github.com>
2022-08-13 10:46:48 +0700

    next

--- base/urls.py
+++ base/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('room/', views.room, name="room"),
     path('niceweb/', views.niceweb, name="niceweb"),
+    path('niceweb2/', views.niceweb2, name="niceweb2"),
+    path('energy/', views.energy, name="energy"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 
@@ -16,3 +17,58 @@ def niceweb(request):
     return render(request, 'niceweb.html')
 
 
+def niceweb2(request):
+    a = 121212
+    z = ''
+    try:
+        if request.method == "POST":
+            n1 = eval(request.POST.get('num1'))
+            n2 = eval(request.POST.get('num2'))
+            n3 = eval(request.POST.get('num3'))
+            z = n1 + n2 + n3
+            opr = request.POST.get('opr')
+            if opr == "+":
+                z = n1 + n2 + n3
+            elif opr == "-":
+                z = n1 - n2 - n3
+            elif opr == "*":
+                z = n1 * n2 * n3
+            elif opr == "/":
+                z = n1 / n2 / n3
+
+    except:
+        z = "Invalid opr ...."
+
+    return render(request, 'niceweb2.html', {'z': z, 'a': a})
+
+
+def energy(request):
+    a = 121212
+    z = ''
+    try:
+        if request.method == "POST":
+            n1 = eval(request.POST.get('num1'))
+            n2 = eval(request.POST.get('num2'))
+            n3 = eval(request.POST.get('num3'))
+            z = n1 + n2 + n3
+            opr = request.POST.get('opr')
+            if opr == "+":
+                z = n1 + n2 + n3
+            elif opr == "-":
+                z = n1 - n2 - n3
+            elif opr == "*":
+                z = n1 * n2 * n3
+            elif opr == "/":
+                z = n1 / n2 / n3
+
+    except:
+        z = "Invalid opr ...."
+
+    return render(request, 'energy.html', {'z': z, 'a': a})
+
+# planck = 6.63
+#
+#
+# def niceweb1_1(request):
+#     context = {"planck": planck,}
+#     return render(request, 'niceweb1.1.html', context)
