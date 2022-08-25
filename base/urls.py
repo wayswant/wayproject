d008d33eaf14726657217882902f679627551c47
wahyukuswant <97098665+wahyukuswant@users.noreply.github.com>
2022-08-25 10:21:58 +0700

    next

--- base/urls.py
+++ base/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    # path('', views.home, name="home"),
     path('room/', views.room, name="room"),
     path('niceweb/', views.niceweb, name="niceweb"),
     path('niceweb2/', views.niceweb2, name="niceweb2"),
     path('energy/', views.energy, name="energy"),
+    path('', views.index, name="index"),
 ]
--- base/views.py
+++ base/views.py
@@ -8,7 +8,7 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'home.html')
 
 
 def room(request):
@@ -118,3 +118,28 @@ def energy(request):
 # def niceweb1_1(request):
 #     context = {"planck": planck,}
 #     return render(request, 'niceweb1.1.html', context)
+
+
+def index(request):
+    a = 121212
+    z = ''
+    try:
+        if request.method == "POST":
+            n1 = eval(request.POST.get('num1'))
+            n2 = eval(request.POST.get('num2'))
+            n3 = eval(request.POST.get('num3'))
+            z = n1 + n2 + n3
+            opr = request.POST.get('opr')
+            if opr == "+":
+                z = n1 + n2 + n3
+            elif opr == "-":
+                z = n1 - n2 - n3
+            elif opr == "*":
+                z = n1 * n2 * n3
+            elif opr == "/":
+                z = n1 / n2 / n3
+
+    except:
+        z = "Invalid opr ...."
+
+    return render(request, 'index.html', {'z': z, 'a': a})
